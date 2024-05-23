b908172b54ba23274d666df49e26605420b13324
Anton Konyk <antonkonyk.com>
2024-05-23 11:56:06 +0200

    add todo_list in INSTALLED_APPS, change TIME_ZONE, add LOGIN_REDIRECT_URL

--- todo/settings.py
+++ todo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "todo_list",
 ]
 
 MIDDLEWARE = [
@@ -105,12 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = "/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
