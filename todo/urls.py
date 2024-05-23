2885eb21db32a7df82e6e6476bc188b4d54f4919
Anton Konyk <antonkonyk.com>
2024-05-23 16:43:14 +0200

    add URL configuration for todo project

--- todo/urls.py
+++ todo/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", include("todo_list.urls", namespace="todo_list")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
